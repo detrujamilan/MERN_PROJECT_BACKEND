4e6d55daaf3f71d41fe7cb4b04bd4c75fbaccb97
milandetruja <milandetruja2@gmail.com>
2024-04-22 16:48:35 +0530

    some issue resolve

--- index.js
+++ index.js
@@ -8,11 +8,19 @@ const api = process.env.APP_URL;
 app.use(express.json())
 app.use(cors())
 
-app.get(api + "/products", (req, res) => {
-    res.send("Hello Api!");
+app.get(`${api}/products`, (req, res) => {
+    const products = {
+        name: "Hair Machine",
+        image: "Some_Url"
+    }
+    res.send(products);
+})
+app.post(`${api}/products`, (req, res) => {
+    const newProducts = req.body;
+    res.send(newProducts);
 })
 
-const port = 3000;
+const port = 3001;
 
 app.listen(port, () => {
     console.log(`Server Is Running https://localhost:${port}`);
