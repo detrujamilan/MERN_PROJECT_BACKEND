0e974904013982979545c4a5780a3209e3303a8a
milandetruja <milandetruja2@gmail.com>
2024-04-28 22:27:44 +0530

    some issue resolve

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -2,10 +2,25 @@ const { expressjwt } = require("express-jwt");
 
 function authJwt() {
   const secret = process.env.secret;
+  const api = process.env.APP_URL;
   return expressjwt({
     secret,
     algorithms: ["HS256"],
-  });
+    isRevoked: isRevoked,
+  }).unless({
+    path: [
+      { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
+      { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
+      `${api}/users/login`,
+      `${api}/users/register`,
+  ]
+  })
 }
 
+async function isRevoked(req, token) {
+  if (!token.payload.isAdmin) {
+    return true;
+  }
+  return false;
+}
 module.exports = authJwt;
